42cb447dd64b7b738b02b20e5a2e9d05c657c7fc
kushalmahawar2005 <kushalmahawar71@gmail.com>
2026-05-21 11:56:24 +0530

    Fix Build Error in Moongoose

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,12 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
 
 interface MongooseCache {
   conn: typeof mongoose | null;
@@ -28,6 +21,14 @@ if (!cached) {
 }
 
 export async function connectToDatabase() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
@@ -37,7 +38,7 @@ export async function connectToDatabase() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongooseInstance) => {
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongooseInstance) => {
       return mongooseInstance;
     });
   }
